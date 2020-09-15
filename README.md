5467c5bbe5b5e93a5fd2172564a219380feda24d
WhatsFish <metralixlhy@163.com>
2020-09-15 21:55:16 +0800

    version1

---
+++ README.md
@@ -0,0 +1,13 @@
+## 获取wiki id的小脚本
+
+* 使用方法：
+> python getid [query_name]
+
+* demo:
+```python
+python getid 香港
+```
+
+* 注：
+
+**挂上梯子，要全局模式**
---
+++ getid.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 18:26:35 2020
+
+@author: User
+"""
+import requests
+import sys
+import json
+
+def get_id(name):
+    S = requests.Session()
+    URL = "https://zh.wikipedia.org/w/api.php"
+    PARAMS = {
+        "action": "query",
+        "titles": name,
+        "prop": "pageprops",
+        "format": "json"
+        }
+    R = S.get(url=URL, params=PARAMS)
+    DATA = R.json()
+    #rint(DATA["query"])
+    pages = DATA["query"]["pages"]
+    for k in pages.keys():
+        info = pages[k]
+        wiki_id = info["pageprops"]["wikibase_item"]
+    print(wiki_id)
+
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) != 2:
+        print("input error!")
+        print("demo:")
+        print("python getid 高永文")
+        sys,exit(0)
+        
+    get_id(sys.argv[1])
